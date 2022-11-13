efba34f6965763424ba4fc350a9fc4847c88e6a2
themohal <farjadmohal@gmail.com>
2022-11-13 05:28:43 +0500

    Week 1 hello world

---
+++ Rust CLI/Week 1/hello-world/target/rls/debug/deps/hello_world-32641d99d36e3439.d
@@ -0,0 +1,5 @@
+d:\Rust\Rust Practice\Week 1\hello-world\target\rls\debug\deps\hello_world-32641d99d36e3439.rmeta: src\main.rs
+
+d:\Rust\Rust Practice\Week 1\hello-world\target\rls\debug\deps\hello_world-32641d99d36e3439.d: src\main.rs
+
+src\main.rs:
---
+++ Rust CLI/Week 1/hello-world/target/rls/debug/deps/hello_world-48649687f5dc1f10.d
@@ -0,0 +1,5 @@
+d:\Rust\Rust Practice\Week 1\hello-world\target\rls\debug\deps\hello_world-48649687f5dc1f10.rmeta: src\main.rs
+
+d:\Rust\Rust Practice\Week 1\hello-world\target\rls\debug\deps\hello_world-48649687f5dc1f10.d: src\main.rs
+
+src\main.rs:
